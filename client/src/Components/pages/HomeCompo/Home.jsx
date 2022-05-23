031647f909e75c5b3c01572360aaaf1782110e7b
ermias-mangisto <ermiasmangisto@gmail.com>
2022-05-23 16:08:23 +0300

    .

--- client/src/Components/pages/HomeCompo/Home.jsx
+++ client/src/Components/pages/HomeCompo/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import "./Home.css"
 
 export default function Home() {
   return (
-    <div>Home</div>
+<>
+
+<div className="hero">
+<video src="media/vid.mp4" 
+className="bg-video"
+ autoPlay muted loop 
+   type="video/mp4">
+</video>
+</div>
+<div>hello</div>
+</>
   )
 }
