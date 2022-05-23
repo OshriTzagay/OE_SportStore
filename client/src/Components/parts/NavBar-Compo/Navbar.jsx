5ec0febf42a08c6beff9d9ba0a27b15cd543940b
OshriTzagay <oshritzagay@gmail.com>
2022-05-23 16:07:51 +0300

    EEaaammaaBardak

--- client/src/Components/parts/NavBar-Compo/Navbar.jsx
+++ client/src/Components/parts/NavBar-Compo/Navbar.jsx
@@ -1,15 +1,60 @@
 import { Link } from "react-router-dom";
-import React from 'react'
-
+import React from "react";
+import "./Nav-Bar.css";
 export default function Navbar() {
   return (
-    <div>
-    <Link to="/">Home|| </Link>
-    <Link to="/cart">Cart|| </Link>
-    <Link to="/shoes">Shoes|| </Link>
-    <Link to="/shirts">Shirts|| </Link>
+    <nav className="nav-bar">
+      <div className="pre-nav">
+        <div className="section-B">
+          <img src="imgs/jordanLogo.png" alt="logo" className="logo" />
+        </div>
+
+        <div className="section-A">
+          <p>Help |</p>
+          <p>Join Us |</p>
+          <p>Sign In |</p>
+        </div>
+      </div>
 
 
-    </div>
-  )
+
+
+
+<div className="nav-routes">
+
+  <div className="left-section"></div>
+
+
+        <ul className="theUl">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="shoes">Shoes</Link>
+        </li>
+        <li>
+          <Link to="/shirts">Shirts</Link>
+        </li>
+
+        <li>
+          <Link to="/accesso">Accessories</Link>
+        </li>
+   
+      </ul>
+
+
+  <div className="right-section">
+    <ul>
+      <li><input type="text" /></li>
+      <li><img src="https://pic.onlinewebfonts.com/svg/img_335286.png" alt="" className="accIcon"/></li>
+      <li></li>
+
+    </ul>
+  </div>
+
+</div>
+
+
+    </nav>
+  );
 }
